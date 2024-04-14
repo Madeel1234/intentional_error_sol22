c6611a0fd7af8de7ab5f2094ffd1a972488abea1
Madeel1234 <164627451+Madeel1234@users.noreply.github.com>
2024-04-14 15:56:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+#! /usr/bin/env node
+//Solution # 22 
+// Intentional error: If you haven't received an array index error in one of your program yet, try to make one happen. 
+//change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+let array = ['Adeel', 'Noushad', 'Aisha', 2, 3, 6, 9, 5];
+console.log(array[5]);
+export {};
---
+++ app.ts
@@ -0,0 +1,9 @@
+#! /usr/bin/env node
+
+//Solution # 22 
+// Intentional error: If you haven't received an array index error in one of your program yet, try to make one happen. 
+//change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+
+let array: (string|number)[]=['Adeel','Noushad','Aisha',2,3,6,9,5]
+console.log(array[5])
